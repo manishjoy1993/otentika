3e500365a9e2f3bcce95465c8fc18760a9185e1e
root <root@host.otentikaweb.com>
2017-04-10 09:30:10 -0400

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git pull origin master
 echo "---------------- pull command complete ----------------"
 php bin/magento setup:upgrade
 php bin/magento setup:static-content:deploy
+php bin/magento setup:static-content:deploy fr_FR
 grunt less:view
 chmod -R 777 var/
 chmod -R 777 pub/
