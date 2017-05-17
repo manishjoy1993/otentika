61a614212f8aa041c1d024bf97daa54069627531
root <root@host.otentikaweb.com>
2017-05-17 06:16:36 -0400

    changes

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ php bin/magento setup:upgrade
 php bin/magento setup:static-content:deploy
 php bin/magento setup:static-content:deploy fr_FR
 grunt less:view
+grunt less:fchild
+grunt less:schild
 php bin/magento cache:flush
 chmod -R 777 var/
 chmod -R 777 pub/
