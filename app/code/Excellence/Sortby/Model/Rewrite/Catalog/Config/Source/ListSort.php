2807025ef37b1cb85baccf07b522a5a2269ea0c4
Manish Prakash <manish@excellencetechnologies.in>
2017-05-25 16:43:07 +0530

    Category page sorter and banner issue solved

--- app/code/Excellence/Sortby/Model/Rewrite/Catalog/Config/Source/ListSort.php
+++ app/code/Excellence/Sortby/Model/Rewrite/Catalog/Config/Source/ListSort.php
@@ -13,7 +13,9 @@ class ListSort extends \Magento\Catalog\Model\Config\Source\ListSort {
         }
         $options[] = ['label' => __('Popularity'), 'value' => 'popularity'];
         $options[] = ['label' => __('Rating'), 'value' => 'rating_summary'];
-        
+				$options[] = ['label' => __('Price (Low to High)'), 'value' => 'price_asc'];
+        $options[] = ['label' => __('Price (High to Low)'), 'value' => 'price_desc'];
+
         return $options;
     }
 }
