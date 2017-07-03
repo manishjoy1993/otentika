745cf8da5b2b7f53e0b2a5a0f3212c06719d712f
Manish Prakash <manish@excellencetechnologies.in>
2017-07-03 11:24:13 +0530

    geoIp switcher

---
+++ app/code/Tobai/GeoStoreSwitcher/Model/Config/Source/Website.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © 2016 ToBai. All rights reserved.
+ */
+namespace Tobai\GeoStoreSwitcher\Model\Config\Source;
+
+class Website implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * @var \Magento\Store\Model\System\Store
+     */
+    protected $store;
+
+    /**
+     * @param \Magento\Store\Model\System\Store $store
+     */
+    public function __construct(
+        \Magento\Store\Model\System\Store $store
+    ) {
+        $this->store = $store;
+    }
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return $this->store->getWebsiteValuesForForm(true, false);
+    }
+}
