a9c4c0280ceff32262965b96a90c10d6a3216170
Manish Joy <manishjoy1993@hotmail.com>
2017-07-07 15:31:43 +0530

    changes

--- app/code/Excellence/Login/Controller/Checkout/Index/Index.php
+++ app/code/Excellence/Login/Controller/Checkout/Index/Index.php
@@ -21,13 +21,15 @@ class Index extends \Magento\Checkout\Controller\Index\Index
                 $resultRedirect->setPath('login');
                 return $resultRedirect;
             }
-            if(empty($this->getRequest()->getParam('username'))){
+            if(empty($this->getRequest()->getParam('username')) && !($this->_customerSession->isLoggedIn())){
                 $resultRedirect = $this->resultRedirectFactory->create();
-                $this->messageManager->addError(__('Please select any of the following method.'));
+                // $this->messageManager->addNotice(__('Please select any of the following method.'));
                 $resultRedirect->setPath('login');
                 return $resultRedirect;
             } else {
-                if(!($this->_coreRegistry->registry('username'))) {
+                if($this->_customerSession->isLoggedIn()) {
+                    $this->_coreRegistry->register('username', $this->_customerSession->getCustomer()->getEmail());
+                } else {
                     $this->_coreRegistry->register('username', $this->getRequest()->getParam('username'));
                 }
             }
--- app/code/Excellence/Login/Controller/Index/Index.php
+++ app/code/Excellence/Login/Controller/Index/Index.php
@@ -17,9 +17,11 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $session,
         PageFactory $resultPageFactory
     ) {
         $this->resultPageFactory = $resultPageFactory;
+        $this->session = $session;
         parent::__construct($context);
     }
 	
@@ -30,6 +32,15 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
+        $customerSession = $this->session;
+        if($customerSession->isLoggedIn()) {
+            $resultRedirect = $this->resultRedirectFactory->create();
+            $customerSession->setCheckoutLoggedIn(true);
+            $email = $customerSession->getCustomer()->getEmail();
+            $customerSession->setCheckoutUsername($email);
+            $resultRedirect->setPath('checkout');
+            return $resultRedirect;
+        }
         /** @var \Magento\Framework\View\Result\Page $resultPage */
         $resultPage = $this->resultPageFactory->create();
         return $resultPage;
